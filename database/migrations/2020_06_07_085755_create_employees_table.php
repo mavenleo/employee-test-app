d3b648a93aad3a5b7026a26c0d05a826105e9297
Lucasbeta <famous.nrv@gmail.com>
2020-06-08 06:32:40 +0100

    initial commit

---
+++ database/migrations/2020_06_07_085755_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('job_title');
+            $table->string('job_type');
+            $table->decimal('current_salary',18,2);
+            $table->string('last_salary_change')->default(0);
+            $table->string('job_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
